b3a60fad95e8e666a86e9a5d6ed304f92669a154
ElianEsparza <128758476+ElianEsparza@users.noreply.github.com>
2023-03-30 18:08:15 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,8 +9,9 @@ void showMenu();
 void getChoice(int& choice, int limit);
 
 void createAcc();
-void loginU();
-void loginP(int lineNum);
+void loginUsername();
+void loginPassword(string tempPassword);
+void incorrectPassword(string tempPassword);
 
 int main()
 {
@@ -28,7 +29,8 @@ int main()
 		break;
 	case 2:
 		cout << "Log In.\n";
-		loginU();//combine loginU and loginP into login(?)
+		cin.ignore();
+		loginUsername();
 		break;
 	case 3:
 		cout << "Exit.\n";
@@ -40,8 +42,6 @@ int main()
 
 void showMenu()
 {
-
-
 	cout << "****************************************\n"
 		<< "Welcome, User." << endl
 		<< "[1] Create Account." << endl
@@ -87,7 +87,7 @@ void createAcc()
 	cout << "Successfully created account." << endl;
 }
 
-void loginU()
+void loginUsername()
 {
 	ifstream accountsFile;
 
@@ -97,72 +97,76 @@ void loginU()
 		cout << "Input file failed to open.";
 		exit(1);
 	}
-
+	accountsFile.seekg(0,ios::beg);
 	string tempUsername;
-	int lineNumber = 0;
+	string tempPassword;
+	int lineNumber;
 	string userInput;
 
-	cout << "Input username: ";
-	cin.ignore();
-	getline(cin, userInput);
-
-	while (getline(accountsFile, tempUsername))
-	{	
-		lineNumber++;
-		if(lineNumber%2==1)
-		if (tempUsername == userInput)
+		cout << "Input username: ";
+		getline(cin, userInput);
+		lineNumber = 0;
+		while (getline(accountsFile, tempUsername))
 		{
-			loginP(lineNumber);
-			accountsFile.close();
-			exit(2);
-		}
+			lineNumber++;
+			if (lineNumber % 2 == 1)
+				if (tempUsername == userInput)
+				{
+					for (int i = 1; i <= lineNumber + 1; i++)
+					{
+						getline(accountsFile, tempPassword);
+						cout << tempPassword;
+						cout << lineNumber;
+					}
+					loginPassword(tempPassword);
+					accountsFile.close();
+					exit(2);
+				}
 
-	}
-	cout << "User does not exist, try again\n";
-	loginU();
+		}
+		cout << "User does not exist, try again.\n";
+		accountsFile.close();
+		loginUsername();
 }
 
-void loginP(int lineNumber)
+void loginPassword(string tempPassword)
 {
-	ifstream accountsFile("accounts.txt");
-	string tempPass;
 	string userInput;
-	int choice;
-
-	for (int i = 1; i <= lineNumber+1; i++)
-	{
-		getline(accountsFile, tempPass);
-	}
 
 	cout << "Input password: ";
 	getline(cin, userInput);
-	if (userInput == tempPass)
+
+	if (userInput == tempPassword)
 	{
 		cout << "Successfully logged in!";
 		exit(2); //Replace with main menu function call.
 	}
 	else
+	incorrectPassword(tempPassword);
+}
+
+void incorrectPassword(string tempPassword)
+{
+	int choice;
+	cout << "Incorrect password." << endl <<
+		"[1] Try again." << endl <<
+		"[2] Back." << endl <<
+		"[3] Exit." << endl;
+
+	getChoice(choice, 3);
+
+	switch (choice)
 	{
-		cout << "Incorrect password." << endl <<
-			"[1] Try again." << endl <<
-			"[2] Back." << endl <<
-			"[3] Exit." << endl;
-
-		cin >> choice;
-
-		switch (choice)
-		{
-		case 1:
-			cout << "Try again.\n";
-			loginP(lineNumber);
-			break;
-		case 2:
-			cout << "Back.\n";
-			showMenu();
-			break;
-		case 3:
-			cout << "Exit.\n";
-			exit(3);
-		}
+	case 1:
+		cout << "Try again.\n";
+		loginPassword(tempPassword);
+		break;
+	case 2:
+		cout << "Back.\n";
+		showMenu();
+		break;
+	case 3:
+		cout << "Exit.\n";
+		exit(3);
 	}
 }
